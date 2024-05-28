d4d8b3daa2abadd0573ff86f8b0288f00bd02fdf
unknown <axelcondori78@gmail.com>
2024-05-28 01:22:11 -0400

    Añadiendo la funcionalidad de subida de imágenes y otras mejoras

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,16 +1,9 @@
-<?php 
-
-class Conexion{
-
-    static public function conectar(){
-
-       
-        $link = new PDO("mysql:host=localhost;dbname=web", "root", "");
-        $link->exec("set names utf8mb4");
-
+<?php
+class Conexion {
+    public static function conectar() {
+        $link = new PDO("mysql:host=localhost;dbname=mi_base_datos", "root", "");
+        $link->exec("set names utf8");
         return $link;
     }
-
 }
-
 ?>
--- vistas/paginas/login.php
+++ vistas/paginas/login.php
@@ -1,61 +1,46 @@
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="../../index2.html"><b>usuario y roles</b>LTE</a>
-        </div>
-        <!-- /.login-logo -->
-        <div class="login-box-body">
-            <p class="login-box-msg">login</p>
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    
+    // Autenticar al usuario
+    $stmt = Conexion::conectar()->prepare("SELECT * FROM usuarios WHERE usuario = :usuario AND password = :password");
+    $stmt->bindParam(":usuario", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+    $stmt->execute();
 
-            <form method="post">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="log_user" placeholder="Email">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="log_pass" placeholder="Password">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <div class="col-xs-8">
-                        <div class="checkbox icheck">
-                            <label>
-                                <input type="checkbox"> Remember Me
-                            </label>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">ingresar</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
+    $usuario = $stmt->fetch();
 
-                <?php 
-
-
-$ingreso=new ctrUsuarios();
-$ingreso->ctrIngresoUsuarios();
-
-
-
-
+    if ($usuario) {
+        $_SESSION["validarSession"] = "ok";
+        $_SESSION["idBackend"] = $usuario["id"];
+        header("Location: index.php?pagina=usuarios");
+    } else {
+        echo "<script>alert('Usuario o contraseña incorrectos');</script>";
+    }
+}
 ?>
 
-
-
-            </form>
-
-
-
-
-
-
-        </div>
-        <!-- /.login-box-body -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="vistas/css/style.css">
+</head>
+<body>
+    <div class="login-box">
+        <form method="post">
+            <div class="form-group">
+                <input type="text" name="email" placeholder="Email" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <button type="submit">Ingresar</button>
+        </form>
     </div>
-
-
 </body>
-
 </html>
